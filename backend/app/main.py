dbe4da08a11d78a3005ffc839e392be05e441551
Anamika Thakur <anamika2642005@gmail.com>
2026-06-12 13:15:46 +0530

    Fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://paper2product.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
